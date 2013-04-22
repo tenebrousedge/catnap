1686f844207a84b2d5915ee63ace268a5445bcbb
Sethrin <tenebrousedge@gmail.com>
2013-04-21 23:26:09 -0700

    Initial commit of Catnap
    stole ApisSource from Copula

---
+++ Model/Datasource/ApisSource.php
@@ -0,0 +1,455 @@
+<?php
+
+/**
+ * Apis DataSource
+ *
+ * */
+App::uses('DataSource', 'Model/Datasource');
+App::uses('HttpSocketOauth', 'Catnap.Network/Http');
+App::uses('HttpSocket', 'Network/Http');
+App::uses('Hash', 'Utility');
+
+class CouchSource extends DataSource {
+
+/**
+ * The description of this data source
+ *
+ * @var string
+ */
+	public $description = 'CouchDb DataSource';
+
+/**
+ * Holds the datasource configuration
+ *
+ * @var array
+ */
+	public $config = array();
+
+/**
+ * Request Logs
+ *
+ * @var array
+ * @access protected
+ */
+	protected $_requestLog = array();
+
+/**
+ * Request Log limit per entry in bytes
+ *
+ * @var integer
+ * @access protected
+ */
+	protected $_logLimitBytes = 5000;
+
+/**
+ * Holds a configuration map
+ *
+ * @var array
+ */
+	public $map = array();
+
+	protected $_sources = null;
+
+	protected $_baseConfig = array(
+		'format' => 'json',
+		'escape' => 'false',
+		'authMethod' => 'http'
+	);
+
+/**
+ * Stores mapping of db actions to http methods
+ * @var type
+ */
+	public $restMap = array(
+		'create' => 'POST',
+		'read' => 'GET',
+		'update' => 'PUT',
+		'delete' => 'DELETE'
+	);
+
+/**
+ * Returns the appropriate transport object for the datasource.
+ *
+ * @param string $authMethod 'OAuth' or 'OAuthV2' return HttpSocketOauth object
+ * @param string|array $url url or array of config options
+ * @return \HttpSocketOauth|\HttpSocket
+ */
+	public function getHttpObject($authMethod, $url = null) {
+		switch ($authMethod) {
+			case 'OAuth':
+			case 'OAuthV2':
+				$Http = new HttpSocketOauth($url);
+				break;
+			default:
+				$Http = new HttpSocket($url);
+				break;
+		}
+		return $Http;
+	}
+
+/**
+ *
+ * @param \Model $model
+ * @return mixed
+ */
+	public function describe(\Model $model) {
+		//TODO: Rewrite this to do something appropriate
+		if (!empty($model->schema)) {
+			$schema = $model->schema;
+		} elseif (!empty($this->_schema[$model->name])) {
+			$schema = $this->_schema[$model->name];
+		} elseif (!empty($this->map[$model->useDbConfig])) {
+			$schema = $this->map[$model->useDbConfig];
+		} else {
+			return null;
+		}
+		return $schema;
+	}
+
+/**
+ *
+ * @param string|array $query array of params to be converted
+ * @param boolean $escape whether to escape the separator
+ * @return string string containing query
+ */
+	protected function _buildQuery($query, $escape = false) {
+		if (is_array($query)) {
+			$query = substr(Router::queryString($query, array(), $escape), '1');
+		}
+		return $query;
+	}
+
+/**
+ * In an ideal world, this would construct a HttpRequest object. Such will likely be the case in CakePHP 3.0
+ *
+ * Currently it returns an array of values to be passed to HttpSocket.
+ *
+ * @param string $apiName
+ * @param string $type
+ * @param array  $request
+ * @return array
+ */
+	protected function _buildRequest($apiName, $type = 'read', $request = array()) {
+		if (empty($this->map)) {
+			$this->map = Configure::read("Catnap.$apiName.path");
+		}
+		$host = Configure::read("Catnap.$apiName.Api");
+		if (is_array($host)) {
+			$this->setConfig($host);
+		}
+		$request['method'] = $this->restMap[$type];
+		$request['uri']['host'] = $this->config['host'];
+		$request['auth'] = $this->_getAuth($this->config['authMethod'], $apiName);
+		if (!empty($this->config['scheme'])) {
+			$request['uri']['scheme'] = $this->config['scheme'];
+		}
+		return $request;
+	}
+
+/**
+ *
+ * @param type $data
+ * @return null
+ */
+	public function listSources($data = null) {
+		if (!empty($this->map)) {
+			foreach ($this->map as $section) {
+				foreach ($section as $endpoint => $contents) {
+					$endpoints[] = $endpoint;
+				}
+			}
+			return array_unique($endpoints);
+		} else {
+			return null;
+		}
+	}
+
+/**
+ * Sends HttpSocket requests. Builds your uri and formats the response too.
+ *
+ * @param string $params
+ * @param array $options
+ * 		method: get, post, delete, put
+ * 		data: either in string form: "option1=foo&option2=bar" or as a keyed array: array('option1' => 'foo', 'option2' => 'bar')
+ * @return HttpSocketResponse $response
+ * @author Dean Sofer
+ */
+	public function request(Model $model) {
+		if (!empty($this->tokens)) {
+			$model->request['uri']['path'] = $this->_swapTokens($model->request['uri']['path'], $this->tokens);
+		}
+
+		if (method_exists($this, 'beforeRequest')) {
+			$model->request = $this->beforeRequest($model);
+		}
+
+		$Http = $this->getHttpObject($this->config['authMethod']);
+		$t = microtime(true);
+
+		$Http->request($model->request);
+
+		$this->took = round((microtime(true) - $t) * 1000, 0);
+		$this->logQuery($Http);
+
+		$model->response = $this->afterRequest($model, $Http->response);
+
+		return $model->response;
+	}
+
+/**
+ *
+ * @param type $query
+ * @param \HttpSocketResponse $response
+ * @return void
+ */
+	public function logQuery(\HttpSocket $Socket) {
+		if (Configure::read('debug')) {
+			$logItems = array($Socket->request['raw'], $Socket->response->raw);
+			foreach ($logItems as &$logPart) {
+				if (strlen($logPart) > $this->_logLimitBytes) {
+					$logPart = substr($logPart, 0, $this->_logLimitBytes) . ' [ ... truncated ...]';
+				}
+			}
+			$newLog = array(
+				'query' => $logItems[0],
+				'response' => $logItems[1],
+				'took' => $this->took,
+			);
+			$this->_requestLog[] = $newLog;
+		}
+	}
+
+/**
+ *
+ * @param type $method
+ * @return array
+ */
+	protected function _getAuth($method, $apiName) {
+		switch ($method) {
+			case 'Basic':
+
+				$auth = array(
+					'method' => 'Basic',
+					'login' => $this->config['login'],
+					'password' => $this->config['password']
+				);
+				break;
+			case 'OAuth':
+				$auth = array(
+					'method' => 'OAuth',
+					'oauth_consumer_key' => $this->config['login'],
+					'oauth_consumer_secret' => $this->config['password']
+						,);
+				$auth['oauth_token'] = (!empty($this->config['access_token'])) ? $this->config['access_token'] : null;
+				$auth['oauth_token_secret'] = (!empty($this->config['token_secret'])) ? $this->config['token_secret'] : null;
+				break;
+			case 'OAuthV2':
+				$auth = array(
+					'method' => 'OAuth',
+					'oauth_version' => '2.0',
+					'client_id' => $this->config['login'],
+					'client_secret' => $this->config['password']
+				);
+				$auth['access_token'] = (isset($this->config['access_token'])) ? $this->config['access_token'] : null;
+				break;
+			default:
+				$auth = null;
+				break;
+		}
+		return array_filter($auth);
+	}
+
+/**
+ * Decodes the response based on the content type
+ *
+ * @param \HttpSocketResponse $response
+ * @return array $response
+ * @author Dean Sofer
+ */
+	public function decode(\HttpSocketResponse $response) {
+		// Get content type header
+		$contentType = explode(';', $response->getHeader('Content-Type'));
+
+		// Decode response according to content type
+		switch ($contentType[0]) {
+			case 'application/xml':
+			case 'application/atom+xml':
+			case 'application/rss+xml':
+				App::uses('Xml', 'Utility');
+				$Xml = Xml::build($response->body());
+				$return = Xml::toArray($Xml);
+				//one of the two lines of code following is unecessary.
+				//Unset will delete the reference and mark the memory for garbage collection.
+				//setting null will clear the memory immediately.
+				//There is some overhead involved in shrinking the stack, so if you are calling this repeatedly it may not be faster.
+				$Xml = null;
+				unset($Xml);
+				break;
+			case 'application/json':
+			case 'application/javascript':
+			case 'text/javascript':
+				$return = json_decode($response->body(), true);
+				break;
+			default:
+				$return = $response->body();
+				break;
+		}
+		return $return;
+	}
+
+/**
+ * Iterates through the tokens (passed or request items) and replaces them into the url
+ *
+ * @param string $url
+ * @param array $tokens optional
+ * @return string $url
+ * @author Dean Sofer
+ */
+	protected function _swapTokens($url, $tokens = array()) {
+		$formattedTokens = array();
+		foreach ($tokens as $token => $value) {
+			$formattedTokens[':' . $token] = $value;
+		}
+		$url = strtr($url, $formattedTokens);
+		return $url;
+	}
+
+/**
+ * Tries iterating through the config map of REST commmands to decide which command to use
+ *
+ * @param string $action
+ * @param string $section
+ * @param array $fields
+ * @return array
+ * @throws CakeException if match not found
+ * @throws CakeException if section not found
+ */
+	protected function _scanMap($action, $section, $fields = array()) {
+		$map = $this->map[$action];
+		if (!Hash::check($map, $section)) {
+			throw new CakeException(__('Section %s not found in Catnap Driver Configuration Map - ', $section) . get_class($this), 500);
+		} else {
+			$element = Hash::extract($map, $section);
+			$path = $required = $optional = null;
+			extract($element);
+			if (array_intersect($fields, $required) == $required) {
+				return compact('path', 'required', 'optional');
+			}
+			throw new CakeException(__('[ApiSource] Could not find a match for passed conditions'), 500);
+		}
+	}
+
+/**
+ * Play nice with the DebugKit
+ *
+ * @param boolean sorted ignored
+ * @param boolean clear will clear the log if set to true (default)
+ * @return array of log requested
+ */
+	public function getLog($sorted = false, $clear = true) {
+		$log = $this->_requestLog;
+		if ($clear) {
+			$this->_requestLog = array();
+		}
+		return array('log' => $log, 'count' => count($log), 'time' => 'Unknown');
+	}
+
+/**
+ * Just-In-Time callback for any last-minute request modifications
+ *
+ * @param Model $model
+ * @return array
+ * @author Dean Sofer
+ */
+	public function beforeRequest(Model $model) {
+		return $model->request;
+	}
+
+	public function afterRequest(Model &$model, HttpSocketResponse &$response) {
+		if (!$response->isOk()) {
+			$model->onError();
+			return false;
+		} else {
+			return $this->decode($response);
+		}
+	}
+
+/**
+ * Uses standard find conditions. Use find('all', $params).
+ *
+ * @param string $model The model being read.
+ * @param string $queryData An array of query data used to find the data you want
+ * @return mixed
+ * @access public
+ */
+	public function read(Model $model, $queryData = array()) {
+		if (!empty($queryData['fields']) && $queryData['fields'] == 'COUNT') {
+			return array(array(array('count' => 1)));
+		}
+		$queryData['conditions'] = (isset($queryData['conditions'])) ? $queryData['conditions'] : array();
+		$model->request = $this->_buildRequest($model->useDbConfig, 'read');
+		$scan = $this->_scanMap('read', $model->useTable, array_keys($queryData['conditions']));
+		$required = $optional = array();
+		extract($scan);
+		$model->request['uri']['path'] = $path;
+		$conditions = array_intersect(array_keys($queryData['conditions']), array_merge($required, $optional));
+		$model->request['uri']['query'] = $this->_buildQuery($conditions);
+		return $this->request($model);
+	}
+
+/**
+ * Sets method = POST in request if not already set
+ *
+ * @param AppModel $model
+ * @param array $fields Unused
+ * @param array $values Unused
+ */
+	public function create(Model $model, $fields = null, $values = null) {
+		$model->request = $this->_buildRequest($model->useDbConfig, 'create');
+		$scan = $this->_scanMap('create', $model->useTable, $fields);
+		extract($scan);
+		$model->request['uri']['path'] = $path;
+		$model->request['body'] = $this->_buildQuery(array_combine($fields, $values), $this->config['escape']);
+		return $this->request($model);
+	}
+
+/**
+ * Sets method = PUT in request if not already set
+ *
+ * @param AppModel $model
+ * @param array $fields Unused
+ * @param array $values Unused
+ */
+	public function update(Model $model, $fields = null, $values = null, $conditions = null) {
+		$model->request = $this->_buildRequest($model->useDbConfig, 'update');
+		$scan = $this->_scanMap('update', $model->useTable, $fields);
+		extract($scan);
+		$model->request['uri']['path'] = $path;
+		$model->request['body'] = $this->_buildQuery(array_combine($fields, $values), $this->config['escape']);
+		return $this->request($model);
+	}
+
+/**
+ * Sets method = DELETE in request if not already set
+ *
+ * @param AppModel $model
+ * @param mixed $id Unused
+ */
+	public function delete(Model $model, $conditions = null) {
+		$model->request = $this->_buildRequest($model->useDbConfig, 'delete');
+		$scan = $this->_scanMap('delete', $model->useTable, array_keys($conditions));
+		extract($scan);
+		$model->request['uri']['path'] = $path;
+		$model->request['body'] = $this->_buildQuery($conditions, $this->config['escape']);
+		return $this->request($model);
+	}
+
+	public function calculate($model, $func, $params = array()) {
+		return 'COUNT';
+	}
+
+	public function getColumnType() {
+		return true;
+	}
+
+}
---
+++ Test/Case/Model/Datasource/ApisSourceTest.php
@@ -0,0 +1,345 @@
+<?php
+
+App::uses('ApisSource', 'Copula.Model/Datasource');
+App::uses('AppModel', 'Model');
+App::uses('PhpReader', 'Configure');
+App::uses('HttpSocketResponse', 'Network/Http');
+App::uses('HttpSocket', 'Network/Http');
+
+class CopulaTestModel extends AppModel {
+
+	public $useDbConfig = "testapi";
+	public $useTable = 'section';
+	public $schema = array(
+		'id' => array(
+			'type' => 'integer',
+			'null' => false,
+			'key' => 'primary',
+			'length' => 11,
+		),
+		'text' => array(
+			'type' => 'string',
+			'null' => true,
+			'length' => 140,
+		),
+		'status' => array(
+			'type' => 'string',
+			'null' => true,
+			'length' => 140,
+		),
+		'customField' => array(
+			'type' => 'string',
+			'null' => true,
+			'length' => 255,
+		),
+	);
+
+}
+
+/**
+ * @property CopulaTestModel $model
+ * @property ApisSource $Apis
+ */
+class ApisSourceTest extends CakeTestCase {
+
+	public $request = array(
+		'method' => 'GET',
+		'uri' => array(
+			'scheme' => 'https',
+			'host' => 'www.example.com',
+			'path' => '/auth/service',
+			'query' => ''
+		),
+		'auth' => array(),
+		'body' => '',
+		'raw' => ''
+	);
+	public $config = array(
+		'version' => '2.0',
+		'scheme' => 'https',
+		'host' => 'www.example.com',
+		'authMethod' => 'OAuthV2',
+		'scheme' => 'https'
+	);
+	public $dbconf = array(
+		'login' => 'login',
+		'password' => 'password',
+		'datasource' => 'Copula.ApisSource'
+	);
+	public $paths = array(
+		'create' => array(
+			'section' => array(
+				'path' => 'somepath',
+				'required' => array(
+					'condition0',
+					'condition1',
+					'condition2'),
+				'optional' => array('optional')
+			)
+		),
+		'read' => array(
+			'section' => array(
+				'path' => 'path_to_enlightenment',
+				'required' => array(
+					'field0',
+					'field1',
+					'field2'),
+				'optional' => array('optional')
+		))
+	);
+	public $Apis = null;
+
+	public function setUp() {
+		parent::setUp();
+		Configure::write('Copula.testapi.path', $this->paths);
+		Configure::write('Copula.testapi.Api', $this->config);
+		$this->Apis = ConnectionManager::create('testapi', $this->dbconf);
+		$this->model = ClassRegistry::init('CopulaTestModel');
+	}
+
+	public function testDescribe() {
+		$results = $this->Apis->describe($this->model);
+		$this->assertEquals($this->model->schema, $results);
+	}
+
+	public function testDescribeMap() {
+		$this->Apis->map['testapi'] = array('create' => array('stuff'));
+		unset($this->model->schema);
+		$results = $this->Apis->describe($this->model);
+		$this->assertEquals($this->Apis->map['testapi'], $results);
+	}
+
+	public function testDescribeNull() {
+		unset($this->model->schema);
+		$this->assertNull($this->Apis->describe($this->model));
+	}
+
+	public function testListSources() {
+		$this->Apis->map['create'] = array('endpoint' => array('path' => 'arbitrary'));
+		$results = $this->Apis->listSources();
+		$this->assertEquals(array('endpoint'), $results);
+		unset($this->Apis->map['create']);
+		$this->assertNull($this->Apis->listSources());
+	}
+
+	public function testBeforeRequest() {
+		$this->model->request = array('request');
+		$this->assertEquals(array('request'), $this->Apis->beforeRequest($this->model));
+	}
+
+	public function testLogQuery() {
+		$t = microtime(true);
+		$Socket = new HttpSocketOauth();
+		$Socket->request['raw'] = 'This is another string';
+		$Socket->response = new HttpSocketResponse();
+		$Socket->response->raw = "This is a string to be logged";
+		Configure::write('debug', '1');
+		$this->Apis->took = round((microtime(true) - $t) * 1000, 0);
+		$this->Apis->logQuery($Socket);
+		$log = $this->Apis->getLog(false);
+		$this->assertEquals($Socket->request['raw'], $log['log'][0]['query']);
+		$this->assertEquals($Socket->response->raw, $log['log'][0]['response']);
+		$this->assertEquals($this->Apis->took, $log['log'][0]['took']);
+		$Socket->request['raw'] .= str_repeat('abcdef', 1000);
+		$this->Apis->logQuery($Socket);
+		$log2 = $this->Apis->getLog(false);
+		$this->assertTrue((substr($log2['log'][0]['query'], '-20')) == '[ ... truncated ...]');
+	}
+
+	public function testDecode() {
+		$response = new HttpSocketResponse();
+		$expected = array('key' => 'value', 'key' => 'value');
+		$response->body = json_encode($expected);
+		$response->headers['Content-Type'] = 'application/json; charset=utf-8';
+		$this->assertEquals($expected, $this->Apis->decode($response));
+		$Xml = Xml::build(file_get_contents(CAKE . 'Test' . DS . 'Fixture' . DS . 'sample.xml'));
+		$anotherExpected = Xml::toArray($Xml);
+		$anotherResponse = new HttpSocketResponse();
+		$anotherResponse->body = file_get_contents(CAKE . 'Test' . DS . 'Fixture' . DS . 'sample.xml');
+		$anotherResponse->headers['Content-Type'] = 'application/xml; charset=utf-8';
+		$this->assertEquals($anotherExpected, $this->Apis->decode($anotherResponse));
+		$moarResponse = new HttpSocketResponse();
+		$response->body = '<!doctype html><html lang=en><head><meta charset=utf-8><title>blah</title></head><body><p>pizza!</p></body></html>';
+		$moarResponse->headers['Content-Type'] = 'text/html; charset=utf-8';
+		$this->assertEquals($moarResponse->body, $this->Apis->decode($moarResponse));
+	}
+
+	public function testGetHttpObject() {
+		$url = 'https://example.com/shop/index.php?product_id=32&highlight=green+dress&cat_id=1&sessionid=123&affid=431';
+		$OauthSocket = $this->Apis->getHttpObject('OAuthV2', $url);
+		$this->assertTrue($OauthSocket instanceof HttpSocketOauth);
+		$HttpSocket = $this->Apis->getHttpObject('Http', $url);
+		$this->assertTrue($HttpSocket instanceof HttpSocket);
+	}
+
+	public function testRead() {
+		$query = array(
+			'conditions' => array(
+				'field0' => 'value0',
+				'field1' => 'value1',
+				'field2' => 'value2'
+			)
+		);
+		unset($this->Apis);
+		$this->Apis = $this->getMock('ApisSource', array('request'), array($this->dbconf));
+		$this->Apis->expects($this->any())
+				->method('request')
+				->will($this->returnArgument(0));
+		$model = $this->Apis->read($this->model, $query);
+		$this->assertFalse(empty($model->request));
+		$this->assertEquals('GET', $model->request['method']);
+		$auth = array('method' => 'OAuth', 'oauth_version' => '2.0', 'client_id' => 'login', 'client_secret' => 'password');
+		$this->assertEquals($auth, $model->request['auth']);
+	}
+
+	public function testReadInvalidSection() {
+		$query = array(
+			'conditions' => array(
+				'field0' => 'value0',
+				'field1' => 'value1',
+				'field2' => 'value2'
+			)
+		);
+		$this->model->useTable = 'notfound';
+		unset($this->Apis);
+		$this->setExpectedException('CakeException');
+		$this->Apis = $this->getMock('ApisSource', array('request'), array($this->dbconf));
+		$this->Apis->expects($this->any())
+				->method('request')
+				->will($this->returnArgument(0));
+		$this->Apis->read($this->model, $query);
+	}
+
+	public function testReadWrongConditions() {
+		$query = array(
+			'section' => 'section',
+			'conditions' => array(
+				'field1' => 'value1',
+				'field2' => 'value2'
+			)
+		);
+		unset($this->Apis);
+		$this->setExpectedException('CakeException');
+		$this->Apis = $this->getMock('ApisSource', array('request'), array($this->dbconf));
+		$this->Apis->expects($this->any())
+				->method('request')
+				->will($this->returnArgument(0));
+		$this->Apis->read($this->model, $query);
+	}
+
+	public function testCreate() {
+		unset($this->Apis);
+		$this->Apis = $this->getMock('ApisSource', array('request'), array($this->dbconf));
+		$this->Apis->expects($this->any())
+				->method('request')
+				->will($this->returnArgument(0));
+		$fields = array('condition0', 'condition1', 'condition2');
+		$values = array('zero', 'one', 'two');
+		$model = $this->Apis->create($this->model, $fields, $values);
+		$this->assertFalse(empty($model->request['body']));
+	}
+
+	public function testRequest() {
+		unset($this->Apis);
+		$this->Apis = $this->getMock('ApisSource', array('getHttpObject'), array($this->dbconf));
+		$this->Http = $this->getMock('HttpSocket');
+		$response = new HttpSocketResponse();
+		$expected = array('key' => 'value', 'key' => 'value');
+		$response->body = json_encode($expected);
+		$response->headers['Content-Type'] = 'application/json; charset=utf-8';
+		$response->code = '200';
+		$this->Http->expects($this->once())
+				->method('request')
+				->will($this->returnValue($response));
+		$this->Http->response = $response;
+		$this->Apis->expects($this->once())
+				->method('getHttpObject')
+				->will($this->returnValue($this->Http));
+		$this->model->request = array();
+		$this->model->request['raw'] = 'This is a test request';
+		Configure::write('debug', '1');
+		$result = $this->Apis->request($this->model);
+		$this->assertFalse(empty($this->Apis->took));
+		$this->assertTrue(is_array($result));
+		$log = $this->Apis->getLog();
+		$this->assertFalse(empty($log['log']));
+	}
+
+	public function testSwapTokens() {
+		$this->Apis = $this->getMock('ApisSource', array('getHttpObject'), array($this->dbconf));
+		$this->Http = $this->getMock('HttpSocket');
+		$response = new HttpSocketResponse();
+		$expected = array('key' => 'value', 'key' => 'value');
+		$response->body = json_encode($expected);
+		$response->headers['Content-Type'] = 'application/json; charset=utf-8';
+		$response->code = '200';
+		$this->Http->expects($this->once())
+				->method('request')
+				->will($this->returnValue($response));
+		$this->Http->response = $response;
+		$this->Apis->expects($this->once())
+				->method('getHttpObject')
+				->will($this->returnValue($this->Http));
+		$this->model->request = array();
+		$this->model->request['uri']['path'] = '/this/:is_the/end/:';
+		$this->Apis->tokens = array('is_the' => '', '' => 'up');
+		$result = $this->Apis->request($this->model);
+		$this->assertEquals('/this//end/up', $this->model->request['uri']['path']);
+	}
+
+	public function testColumnType() {
+		$this->assertTrue($this->Apis->getColumnType());
+	}
+
+	public function testCalculate() {
+		$this->assertEquals('COUNT', $this->Apis->calculate($this->model, 'string'));
+	}
+
+	public function testCount() {
+		$expected = array(array(array('count' => 1)));
+		$results = $this->Apis->read($this->model, array('fields' => 'COUNT'));
+		$this->assertEquals($expected, $results);
+	}
+
+	public function testAfterRequestFailed() {
+		$response = new HttpSocketResponse();
+		$response->code = 403;
+		$this->assertFalse($this->Apis->afterRequest($this->model, $response));
+	}
+
+	public function testDelete() {
+		$this->Apis = $this->getMock('ApisSource', array('request'), array($this->dbconf));
+		$this->Apis->expects($this->any())
+				->method('request')
+				->will($this->returnArgument(0));
+		$conditions = array('field0' => 'value');
+		$this->Apis->map['delete'] = $this->paths['read'];
+		$this->Apis->map['delete']['section']['required'] = array();
+		$model = $this->Apis->delete($this->model, $conditions);
+		$this->assertEquals('field0=value', $model->request['body']);
+	}
+
+	public function testUpdate() {
+		$this->Apis = $this->getMock('ApisSource', array('request'), array($this->dbconf));
+		$this->Apis->expects($this->any())
+				->method('request')
+				->will($this->returnArgument(0));
+		$conditions = array('field0' => 'value');
+		$fields = array('condition0', 'condition1', 'condition2');
+		$values = array('zero', 'one', 'two');
+		$this->Apis->map['update'] = $this->paths['read'];
+		$this->Apis->map['update']['section']['required'] = array();
+		$model = $this->Apis->update($this->model, $fields, $values, $conditions);
+		$this->assertEquals('condition0=zero&condition1=one&condition2=two', $model->request['body']);
+	}
+
+	public function tearDown() {
+		ConnectionManager::drop('testapi');
+		unset($this->model, $this->Apis);
+		ClassRegistry::flush();
+		parent::tearDown();
+	}
+
+}
+
+?>
